df69508bb2cd1c6f1ebf43db9f430624816b161a
Ctarron <78960806+Ctarron@users.noreply.github.com>
2024-06-07 17:59:59 +0200

    Ez cactai

--- Save0/main.py
+++ Save0/main.py
@@ -1,49 +1,3 @@
-def harvest_sunflower():
-	#Usar siempre a la izquierda
-	size = get_world_size()
-	x = 0
-	move(East)
-	while get_pos_x() != 0:
-		if get_entity_type() != Entities.Sunflower:
-			break
-		x += 1
-		move(East)
-	
-	go_to(0,0)
-	
-	sunflowers = {}
-	
-	for i in range(x):
-		while measure() == None:
-			move(North)
-			if get_pos_y() == 0:
-				move(East)
-				
-		petals = measure() 
-		if not petals in sunflowers:
-			sunflowers[petals] = []
-		sunflowers[petals].append([get_pos_x(),get_pos_y()])
-		move(North)
-		while get_pos_y() != 0:
-			if measure() == None:
-				move(North)
-				continue
-			petals = measure() 
-			if not petals in sunflowers:
-				sunflowers[petals] = []
-			sunflowers[petals].append([get_pos_x(),get_pos_y()]) 
-			move(North)
-		move(East)
-	while sunflowers:
-		max_key = max(sunflowers)
-		if not sunflowers[max_key]:
-			sunflowers.pop(max_key)
-			continue
-		max_petals = sunflowers[max_key].pop()
-		go_to(max_petals[0], max_petals[1])
-		harvest()
-	go_to(0,0)
-
 def do_column(crop, c=1):
 	r = 0
 	for i in range(c):
@@ -81,21 +35,20 @@ go_to(0,0)
 
 clear()
 
+farm_cactus(500)
+
 while True:
 	go_to(0,0)
-	create_maze(500)
-	continue
+	create_maze(5)
 	do_full_sunflower_field()
 	
 	do_column(do_pumpkin, 10)
 	
 	do_column(do_grass, 4)
 	do_column(do_wood, 6)
-
-	do_column(do_grass, 10)
-	
-	create_maze()
 	
 	do_column(do_carrot, 5)
 	do_column(do_grass, 2)
 	do_column(do_wood, 3)	
+	
+	do_column(do_grass, 10)
--- Save0/sunflowers.py
+++ Save0/sunflowers.py
@@ -1,55 +1,8 @@
-
-def do_sunflower(r=1):
+def do_sunflower(r=1):
 	if get_ground_type() != Grounds.Soil:
 		till()
 	trade(Items.Sunflower_Seed)
 	plant(Entities.Sunflower)
-
-def harvest_sunflower():
-	#Usar siempre a la izquierda
-	size = get_world_size()
-	x = 0
-	move(East)
-	while get_pos_x() != 0:
-		if get_entity_type() != Entities.Sunflower:
-			break
-		x += 1
-		move(East)
-	
-	go_to(0,0)
-	
-	sunflowers = {}
-	
-	for i in range(x):
-		while measure() == None:
-			move(North)
-			if get_pos_y() == 0:
-				move(East)
-				
-		petals = measure() 
-		if not petals in sunflowers:
-			sunflowers[petals] = []
-		sunflowers[petals].append([get_pos_x(),get_pos_y()])
-		move(North)
-		while get_pos_y() != 0:
-			if measure() == None:
-				move(North)
-				continue
-			petals = measure() 
-			if not petals in sunflowers:
-				sunflowers[petals] = []
-			sunflowers[petals].append([get_pos_x(),get_pos_y()]) 
-			move(North)
-		move(East)
-	while sunflowers:
-		max_key = max(sunflowers)
-		if not sunflowers[max_key]:
-			sunflowers.pop(max_key)
-			continue
-		max_petals = sunflowers[max_key].pop()
-		go_to(max_petals[0], max_petals[1])
-		harvest()
-	go_to(0,0)
 	
 def do_full_sunflower_field(size_x = get_world_size(), size_y = get_world_size()):
 	sunflowers = {}
